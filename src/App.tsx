9b3685bf6e96be4ff1f9273a0f8a8286364278fe
Mai Son Viet <sonviet7052006@gamil.com>
2025-10-10 14:55:25 +0700

    sv

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,4 @@
-import React, { useState } from "react";
+import { useState } from "react";
 import { Routes, Route } from "react-router-dom";
 import MapComponent from "../getData/map/MapComponent";
 import WeatherLocation from "./wearther/WeatherLocation";
--- src/wearther/WeatherLocation.tsx
+++ src/wearther/WeatherLocation.tsx
@@ -1,4 +1,4 @@
-import React, { useEffect, useState } from "react";
+import { useEffect, useState } from "react";
 import { countryToLang, weatherTranslations } from "../../transalte/vi/translate";
 import { countryNames } from "../../transalte/countryName/coutryName";
 
